2d79daa570d8fd3640b719c53fada227224e46ac
buck54321 <buck54321@gmail.com>
2017-01-24 18:45:56 -0600

    Project cleanup

--- config (copy).py
+++
@@ -1,5 +0,0 @@
-system = {'hasDisplay':True, 
-			 'qeDir':'/home/buck54321/quantum-espresso', 
-			 'pgmDir':'/home/buck54321/quantum-espresso/files/bin', 
-			 'pseudoDir':'/home/buck54321/quantum-espresso/files/pseudo',
-			}
